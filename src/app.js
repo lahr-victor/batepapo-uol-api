5cbac3873246f6e8384d59cfae004a5011e84fec
Victor Lahr <lahr.victor@gmail.com>
2023-04-17 17:52:47 -0300

    feat: implement participant sign in POST request

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 // PACKAGE IMPORTS
 import cors from 'cors';
+import dayjs from 'dayjs';
 import dotenv from 'dotenv';
 import express from 'express';
+import joi from 'joi';
 import { MongoClient } from 'mongodb';
 
 // SERVER CONFIG
@@ -19,6 +21,38 @@ mongoClient.connect()
 
 // GLOBAL CONSTANTS
 const PORT_NUMBER = 5000;
+
+// ENDPOINTS
+app.post('/participants', async (req, res) => {
+  const { name } = req.body;
+  const participantSchema = joi.object({
+    name: joi.string().min(1).required(),
+  });
+
+  const validation = participantSchema.validate({ name }, { abortEarly: false });
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const participant = await db.collection('participants').findOne({ name });
+    if (participant) return res.sendStatus(409);
+
+    await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
+    await db.collection('messages').insertOne({
+      from: name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss'),
+    });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 
 // FUNCTIONS
 app.listen(PORT_NUMBER, () => console.log(`Running server on port ${PORT_NUMBER}`));
